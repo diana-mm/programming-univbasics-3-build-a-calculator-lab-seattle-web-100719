2bb9656761daba99b4102ea34dccc6cfd598737d
Diana M <dmmiller85@gmail.com>
2019-10-06 19:29:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,20 +3,21 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-puts = num1 + num2 
+puts = num1 - num2 
 end
 
-def diviputs = num1 + num2 
+def division(num1,num2)
+  puts = num1 / num2 
 end
 
 def multiplication(num1, num2)
-
+  puts = num1 * num2 
 end
 
 def modulo(num1, num2)
-
+  puts = num1 / num2 
 end
 
 def square_root(num)
-
+  puts = Math.sqrt(num)
 end
