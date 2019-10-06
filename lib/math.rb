e7b8d5bb5094b3ad1e59acca49150abe2ec3e796
Diana M <dmmiller85@gmail.com>
2019-10-06 19:27:46 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,12 @@
 def addition(num1, num2)
-
+  puts = num1 + num2 
 end
 
 def subtraction(num1, num2)
-
+puts = num1 + num2 
 end
 
-def division(num1, num2)
-
+def diviputs = num1 + num2 
 end
 
 def multiplication(num1, num2)
